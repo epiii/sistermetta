600e5edcdc4a3dadceb346bd6e84c6cd0a574ef2
asrifin <asrifin_ilham@yahoo.com>
2015-05-31 12:35:38 +0700

    hapus file ga dipake

--- libX/dbDump/20150321/pus_generate_kode.sql
+++
@@ -1,8 +0,0 @@
-﻿
-SELECT
-	tanggal1 tgl_pinjam,
-    tanggal2 tgl_pengembalian,
-    if(mtipe=1,"siswa"(if(mtipe=2,"guru","member_luar")))
-FROM pus_peminjaman pj
-LEFT JOIN aka_siswa as ON as.replid = pj.mtipe
- 
--- libX/dblogout.php
+++
@@ -1,6 +0,0 @@
-<?php
-	session_start();
-	session_destroy();
-	$out = (empty($_SESSION))?1:0;
-	echo $out;
-?>
