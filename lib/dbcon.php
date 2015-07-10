8d5c3eeaaac88403756777b74452d4233da2d09e
epiii <gue.elfrianto@gmail.com>
2015-07-11 00:38:03 +0700

    connection db

--- lib/dbcon.php
+++ lib/dbcon.php
@@ -1,17 +1,8 @@
 <?php
-	// local
 	$server   = "localhost";
 	$username = "root";
 	$password = "";
-	// $database = "sister_siadu_demo";
-	$database = "sistermeta";
-	
-	//online
-	// $server   = "mysql.idhotinger.com";
-	// $username = "u484251826_nyuek";
-	// $password = "";
-	// $database = "u484251826_nyuek";
-
+	$database = "sistermetta";
 	mysql_connect($server,$username,$password) or die("Koneksi gagal");
 	mysql_select_db($database) or die("Database tidak bisa dibuka");
 ?>
