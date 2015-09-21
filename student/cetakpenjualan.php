d2896decc941d38cc3b17dffd3ce015a315120af
rekysyarifudin <reky@teamworks.co.id>
2015-09-21 11:55:07 +0700

    21/09/2015

--- student/cetakpenjualan.php
+++ student/cetakpenjualan.php
@@ -182,7 +182,6 @@ $harga = $datas2['harga'];
 $hargabeli = $datas2['hargabeli'];
 $subdiscount = $datas2['subdiscount'];
 $subtotal = $datas2['subtotal'];
-$hargabeli = $hargabeli-($hargabeli*($subdiscount/100));
 $subhargabeli = ($hargabeli*$jumlah);
 if($carabayar=='Piutang'){
 $subtotalbayar ='0';	
