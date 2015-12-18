73e448d72c3ff250483f1d971c473c56261edc7a
epiii <gue.elfrianto@gmail.com>
2015-12-18 09:34:16 +0700

    view_siswa_kelas

---
+++ lib/dbDump/NEW (dr. tim programmer)/#views/vw_siswa_kelas.sql
@@ -0,0 +1,6 @@
+INSERT INTO `vw_siswa_kelas` VALUES (193, 55, 2, 1, 4, 1, 5, 1, 'Lunabelle Veloxia Taslim', '7', '', 'Metta School', '2015', '1', 'Toddler', 'A');
+INSERT INTO `vw_siswa_kelas` VALUES (230, 56, 2, 1, 4, 1, 5, 1, 'Archie Oesjady', '10', '', 'Metta School', '2015', '1', 'Toddler', 'A');
+INSERT INTO `vw_siswa_kelas` VALUES (233, 57, 2, 1, 4, 1, 5, 1, 'Justin Ong', '99', '', 'Metta School', '2015', '1', 'Toddler', 'A');
+INSERT INTO `vw_siswa_kelas` VALUES (238, 58, 2, 1, 4, 1, 5, 1, 'Aldrin Reynard Herdianto', '9', '', 'Metta School', '2015', '1', 'Toddler', 'A');
+INSERT INTO `vw_siswa_kelas` VALUES (240, 59, 2, 1, 4, 1, 5, 1, 'Aaron William Soemarno', '8', '', 'Metta School', '2015', '1', 'Toddler', 'A');
+INSERT INTO `vw_siswa_kelas` VALUES (205, 54, 159, 3, 5, 2, 12, 1, 'vincenzo christopher wen', '93', '', 'Metta School', '2016', '1', 'Pre School', '1');
